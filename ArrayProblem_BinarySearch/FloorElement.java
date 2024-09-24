8b642df6a07c0a4da2aed3763ff08b1bc2c3d99a
AdityaVerma <adityaverma8392@gmail.com>
2024-09-24 19:30:04 +0530

    Add files added

--- ArrayProblem_BinarySearch/FloorElement.java
+++ ArrayProblem_BinarySearch/FloorElement.java
@@ -1,26 +1,24 @@
-
-// Problem: 35 leetcode find Insert Positions
-
-
 package ArrayProblem_BinarySearch;
 
-public class CeilingElement {
-    public static int findCeiling(int[] array, int target) {
+public class FloorElement {
+    public static int findFloor(int[] array, int target) {
         int start = 0;
         int end = array.length - 1;
         while (start <= end) {
             int mid = start + (end - start) / 2;
-            if (array[mid] >= target)
-                end = mid - 1;
-            else
+            if (array[mid] <= target)
                 start = mid + 1;
+            else
+                end = mid - 1;
+
         }
-        return array[start];
+
+        return array[end];
     }
 
     public static void main(String[] args) {
         int[] array = { 2, 3, 5, 9, 14, 16, 18 };
         int target = 15;
-        System.out.println(findCeiling(array, target));
+        System.out.println(findFloor(array, target));
     }
 }
