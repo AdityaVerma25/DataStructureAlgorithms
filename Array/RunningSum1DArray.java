274380c672333aedf2aff268c0d11d4227dd904a
AdityaVerma <adityaverma8392@gmail.com>
2024-10-03 16:06:36 +0530

    Running Sum of 1D Array code Added

---
+++ Array/RunningSum1DArray.java
@@ -0,0 +1,17 @@
+public class RunningSum1DArray {
+    public static int[] runningSum(int[] nums) {
+        int[] ans = new int[nums.length];
+        ans[0] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            ans[i] = ans[i - 1] + nums[i];
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        int[] array = { 1, 2, 3, 4 };
+        int[] result = runningSum(array);
+        for (int i : result) {
+            System.out.print(i + " ");
+        }
+    }
+}
