e0f828a2c1636adab5c27f81ba71218c933960f6
AdityaVerma <adityaverma8392@gmail.com>
2024-10-03 15:50:12 +0530

    ConcatArray problem code added

---
+++ Array/ConcatArray.java
@@ -0,0 +1,24 @@
+
+//https://leetcode.com/problems/concatenation-of-array/submissions/1410327018/
+public class ConcatArray {
+    public static int[] cancatArray(int[] array, int n) {
+        int[] ans = new int[1 * n];
+        for (int i = 0; i < n; i++) {
+            ans[i] = array[i];
+        }
+        for (int i = 0; i < n; i++) {
+            ans[i + n] = array[i];
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 1, 2, 3 };
+        int n = arr.length;
+        int[] result = cancatArray(arr, n);
+        for (int i = 0; i < result.length; i++) {
+            System.out.print(result[i] + " ");
+        }
+        System.out.println();
+    }
+}
