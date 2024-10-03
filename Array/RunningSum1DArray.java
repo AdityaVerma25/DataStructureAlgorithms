3f07ff963a5c3d415f23517b768e37e6ed451207
AdityaVerma <adityaverma8392@gmail.com>
2024-10-03 16:07:36 +0530

    Modified

--- Array/RunningSum1DArray.java
+++ Array/RunningSum1DArray.java
@@ -1,3 +1,5 @@
+// https://leetcode.com/problems/running-sum-of-1d-array/
+
 public class RunningSum1DArray {
     public static int[] runningSum(int[] nums) {
         int[] ans = new int[nums.length];
