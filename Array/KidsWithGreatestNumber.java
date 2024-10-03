a5298906981bcbfbb2846f7ad692b5e3e5d5206e
AdityaVerma <adityaverma8392@gmail.com>
2024-10-03 16:53:39 +0530

    Kids with Greatest Number Candies's code and class added.

---
+++ Array/KidsWithGreatestNumber.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class KidsWithGreatestNumber {
+    public static List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+
+        List<Boolean> result = new ArrayList<>();
+        int max = Integer.MIN_VALUE;
+        for (int i : candies) {
+            if (i > max)
+                max = i;
+        }
+        for (int j : candies) {
+            if (j + extraCandies >= max)
+                result.add(true);
+            else result.add(false);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] candies = { 2, 3, 5, 1, 3 };
+        int extraCandies = 3;
+        List<Boolean> ans = kidsWithCandies(candies, extraCandies);
+        for (boolean b : ans) {
+            System.out.print(b + " ");
+        } 
+    }
+
+}
